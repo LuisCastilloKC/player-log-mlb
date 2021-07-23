55986582c0c7a48f3732d2603b978e28032cc8a6
Luis Castillo <mr.luiscastillo@gmail.com>
2021-07-23 17:50:19 -0400

    add Player class

---
+++ frontend/src/player.js
@@ -0,0 +1,14 @@
+class Player{
+    cconstructor(name, age, gender, team_name, hight, birth_country, position, jersey_number){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.team_name = team_name;
+        this.hight = hight;
+        this.birth_country = birth_country;
+        this.position = position;
+        this.jersey_number = jersey_number
+    }
+};
+
+
