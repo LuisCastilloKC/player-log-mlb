59901413faa66ddc8135d88af3fc48b0bf388edb
Luis Castillo <mr.luiscastillo@gmail.com>
2021-07-23 18:11:08 -0400

    fetching teams data

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const BASE_URL = "http://127.0.0.1:3000"
 
 const fetchTeams = () =>{
-    fetch()
+    fetch(`${BASE_URL}/teams`) // fetching all the data from teams route
     .then()
     .then()
 }
