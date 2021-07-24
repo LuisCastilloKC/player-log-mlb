adf88b18515f2bfef12d9394cc81912f308887c3
Luis Castillo <mr.luiscastillo@gmail.com>
2021-07-23 22:18:05 -0400

    render teams to index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const BASE_URL = "http://127.0.0.1:3000"
 
+//READ showing to the teams index
 const fetchTeams = () =>{
     fetch(`${BASE_URL}/teams`) // fetching all the data from teams route
     .then(resp => resp.json())
     .then(teams => {
 
         for (const team of teams){
-            console.log("railsobj", team)
             let tm = new Team(team.id, team.name)
-            console.log("js obj", tm)
+            tm.renderTeam();
         }
     
     })
