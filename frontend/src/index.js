060305112880ed3890411f6deccf44ec49a33339
Luis Castillo <mr.luiscastillo@gmail.com>
2021-07-23 21:40:49 -0400

    grab obj js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    fetchTeams()
 });
 
 const BASE_URL = "http://127.0.0.1:3000"
 
 const fetchTeams = () =>{
     fetch(`${BASE_URL}/teams`) // fetching all the data from teams route
-    .then()
-    .then()
+    .then(resp => resp.json())
+    .then(teams => {
+
+        for (const team of teams){
+            console.log("railsobj", team)
+            let tm = new Team(team.id, team.name)
+            console.log("js obj", tm)
+        }
+    
+    })
 }
