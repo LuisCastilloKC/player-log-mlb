6549df6505ac4124b466ea5af25f778aebe1c37a
Luis Castillo <mr.luiscastillo@gmail.com>
2021-07-29 23:50:17 -0400

    add CRUD to backend player controller

--- backend/app/controllers/players_controller.rb
+++ backend/app/controllers/players_controller.rb
@@ -1,8 +1,56 @@
 class PlayersController < ApplicationController
 
+    before_action :set_player, only: [:show, :update, :destroy]
+    
+    # GET /players
     def index
         players = Player.all
-        render :json => players
+        render json: players
     end
+    
+  
+    # GET /players/1
+    def show
+      player = Player.find_by(id: params[:id])
+      render json: player
+    end
+  
+    # POST /players
+    def create
+      player = Player.new(team_params)
+  
+      if player.save
+        render json: player, status: :created, location: player
+      else
+        render json: player.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /players/1
+    def update
+      if player.update(player_params)
+        render json: player
+      else
+        render json: player.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /players/1
+    def destroy
+      player = Player.find_by(id: params[:id])
+      player.destroy
+     #byebug
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_player
+        player = Player.find(params[:id])
+      end
+  
+      # Only allow a list of trusted parameters through.
+      def player_params
+        params.require(:player).permit(:name, :age)
+      end
 
 end
