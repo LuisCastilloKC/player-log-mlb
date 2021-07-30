52ee108dd8ea04c0f9018673ae7509921ac2dd6d
Luis Castillo <mr.luiscastillo@gmail.com>
2021-07-30 18:39:10 -0400

    add submit successfull

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,90 +1,102 @@
 const teamForm = document.getElementById("team-form")
+const teamInput = document.getElementById("team-input")
+
 teamForm.addEventListener("submit", submitTeam)
-const submitForm = () => {
-    
+
+
+function submitTeam(){
+    event.preventDefault()
+    console.log(teamInput.value)
 }
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    fetchTeams()
-    createForm()
-     fetchPlayer()
-    playerForm()
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     fetchTeams()
+//     createForm()
+//      fetchPlayer()
+//     playerForm()
+// });
 
-const BASE_URL = "http://127.0.0.1:3000"
+// const BASE_URL = "http://127.0.0.1:3000"
 
 
-//READ - showing to the teams index
-const fetchTeams = () =>{
-    fetch(`${BASE_URL}/teams`) // fetching all the data from teams route
-    .then(resp => resp.json())
-    .then(teams => {
+// //READ - showing to the teams index
+// const fetchTeams = () =>{
+//     fetch(`${BASE_URL}/teams`) // fetching all the data from teams route
+//     .then(resp => resp.json())
+//     .then(teams => {
     
-        for (const team of teams){
-            let tm = new Team(team.id, team.name)
-            tm.renderTeam();
-        }
+//         for (const team of teams){
+//             let tm = new Team(team.id, team.name)
+//             tm.renderTeam();
+//         }
         
-    })
-}
+//     })
+// }
 
 
-//CREAT 
-const createForm = () => {
-    let teamCreate = document.getElementById("create-div")
-    teamCreate.innerHTML +=
-    `
-    <h2>Create Team Name</h2>
-    <form>
-    <label for="name">Team Name:</label>
-    <input type="text" name="name" id="name" ><br>
-    <button type="submit">Create</button>
-    </form>
-    `
+// //CREAT 
+// const createForm = () => {
+//     let teamCreate = document.getElementById("create-div")
+//     teamCreate.innerHTML +=
+//     `
+//     <h2>Create Team Name</h2>
+//     <form>
+//     <label for="name">Team Name:</label>
+//     <input type="text" name="name" id="name" ><br>
+//     <button type="submit">Create</button>
+//     </form>
+//     `
     
-    teamCreate.addEventListener("submit", teamCreateSubmit)
-}
+//     teamCreate.addEventListener("submit", teamCreateSubmit)
+// }
 
-const teamCreateSubmit = () => {
-    event.preventDefault();
-    let name = document.getElementById("name").value
+// const teamCreateSubmit = () => {
+//     event.preventDefault();
+//     let name = document.getElementById("name").value
     
-    let team = {
-        name: name,
-    }
-    clearFields()
-    fetch(`${BASE_URL}/teams`, {
-        method: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-type': 'application/json' 
-        },
-        body: JSON.stringify(team)
-    })
-    .then(resp => resp.json())
-    .then(team => {
-        let tm = new Team(team.id, team.name)
-        tm.renderTeam();
-    })
-}
+//     let team = {
+//         name: name,
+//     }
+//     clearFields()
+//     fetch(`${BASE_URL}/teams`, {
+//         method: "POST",
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-type': 'application/json' 
+//         },
+//         body: JSON.stringify(team)
+//     })
+//     .then(resp => resp.json())
+//     .then(team => {
+//         let tm = new Team(team.id, team.name)
+//         tm.renderTeam();
+//     })
+// }
 
-// This function work but it clear the value -- will come back later
-const clearFields = () =>{
-    document.getElementById("name").value=""
+// // This function work but it clear the value -- will come back later
+// const clearFields = () =>{
+//     document.getElementById("name").value=""
     
-}
+// }
 
-// DELETE
-const deleteTeam = () =>{
-    let teamId = parseInt(event.target.dataset.id)
-    document.getElementById(`team${teamId}`).remove()
-    fetch(`${BASE_URL}/teams/${teamId}`,{
-        method: 'DELETE'
-    })
+// // DELETE
+// const deleteTeam = () =>{
+//     let teamId = parseInt(event.target.dataset.id)
+//     document.getElementById(`team${teamId}`).remove()
+//     fetch(`${BASE_URL}/teams/${teamId}`,{
+//         method: 'DELETE'
+//     })
     
     
-}
+// }
+
+
+
+
+
+
+
 
 
 // const PLAYER_API = "http://127.0.0.1:3000"
