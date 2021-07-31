5eeb6aad00ac7c6ad90564fbe0ee2488b3097af0
Luis Castillo <mr.luiscastillo@gmail.com>
2021-07-30 23:23:06 -0400

    remove default comments from backend controller

--- backend/app/controllers/players_controller.rb
+++ backend/app/controllers/players_controller.rb
@@ -1,20 +1,10 @@
 class PlayersController < ApplicationController
-    #before_action :set_player, only: [:show, :update, :destroy]
     
-    # GET /players
     def index
         players = Player.all
         render json: players
     end
     
-  
-    # GET /players/1
-    def show
-      player = Player.find_by(id: params[:id])
-      render json: player
-    end
-  
-    # POST /players
     def create
       player = Player.new(player_params)
       if player.save
@@ -22,21 +12,18 @@ class PlayersController < ApplicationController
       end
     end
   
-    
-    # DELETE /players/1
     def destroy
       player = Player.find_by(id: params[:id])
       player.destroy
-     #byebug
     end
   
     private
-      # Use callbacks to share common setup or constraints between actions.
+      
       def set_player
         player = Player.find(params[:id])
       end
   
-      # Only allow a list of trusted parameters through.
+      
       def player_params
         params.require(:player).permit(:name, :team_id)
       end
--- backend/app/controllers/teams_controller.rb
+++ backend/app/controllers/teams_controller.rb
@@ -1,19 +1,10 @@
 class TeamsController < ApplicationController
- before_action :set_team, only: [:show, :update, :destroy]
 
-  # GET /teams
   def index
     teams = Team.all
     render json: teams
   end
 
-  # GET /teams/1
-  def show
-    team = Team.find_by(id: params[:id])
-    render json: team
-  end
-
-  # POST /teams
   def create
     team = Team.new(team_params)
     if team.save
@@ -21,29 +12,17 @@ class TeamsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /teams/1
-  def update
-    if team.update(team_params)
-      render json: team
-    else
-      render json: team.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /teams/1
   def destroy
     team = Team.find_by(id: params[:id])
     team.destroy
-   #byebug
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+   
     def set_team
       team = Team.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def team_params
       params.require(:team).permit(:name)
     end
